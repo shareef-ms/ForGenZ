290d04bee91bcabf9d655f6478ed2f818d2245a1
shareef-msm <shareefms2379@gmail.com>
2026-03-24 16:27:33 +0530

    fix: enable mobile network access by updating API detection and server listening

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from './ThemeContext';
 
+// Standard React entry point - no errors here!
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
